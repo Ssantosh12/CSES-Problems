61876fcbe9ff483b2c36bb67d04fadf4cfe45fb5
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-04-20 08:28:58 +0530

    Update and rename main.cpp to Repititions.cpp

--- Repititions.cpp
+++ Repititions.cpp
@@ -12,17 +12,20 @@ int main(){
     for(i=0;i<s.size();i++){
         if(s[i]==prv){
             cnt++;
+            //1st breaking point-> if we reach last element
             if(i==(s.size()-1)){
                 if(cnt>mx)
                     mx=cnt;
             }
         }
         else{
+            //2nd breaking point->continuous same elments chain breaks
             if(cnt>mx)
                 mx=cnt;
             cnt=1;
             prv=s[i];
         }
     }
+    //in each breaking points we send the prv cnt to check if it fits the requirements and then update the cnt
     printf("%d\n",mx);
 }
