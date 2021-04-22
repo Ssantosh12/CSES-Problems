a8e413b2c61cee0c1549441880312bb30aeb28b8
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-04-22 15:59:08 +0530

    Update Coin Piles.cpp

--- Introductory Problems/Coin Piles.cpp
+++ Introductory Problems/Coin Piles.cpp
@@ -8,7 +8,8 @@ int main(){
         int diff,a,b,mn,tim;
         scanf("%d%d",&a,&b);
         string ans="NO";
-      
+        //idea is think of the smalllest they can be so that they both reduces to zero-> 1,2 or 2,1 or 3,3
+        //we can choose other approaches as well, here we go for 3,3-> if they are equal and multiple of 3
         //first we make a and b equal and after becoming equal if a%3==0 (a and b are same) then YES
        // 'diff' times we reduce 2 from bigger one and 1 from smaller one to make them equal (ex. 7 4->diff=3->(7-6) & (4-3)-> 1 1) 
         if(a>b){
