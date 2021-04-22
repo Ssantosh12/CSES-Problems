d6a98c3595dc25e8b5e25d2644a722dfd1066631
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-04-22 08:28:17 +0530

    Update Distinct Numbers.cpp

--- Sorting and Searching/Distinct Numbers.cpp
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int cnt=1,n,i,prv;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    //now we sort the array so that same elements come together
+
+    sort(a,a+n);
+    prv=a[0];
+    for(i=1;i<n;i++){
+        if(a[i]!=prv){
+            cnt++;
+            prv=a[i];
+        }
+    }
+    printf("%d\n",cnt);
+}
