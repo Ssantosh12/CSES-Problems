cba5771021101613375e3b608722605163d67cdd
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-02-23 19:43:42 +0530

    Update Apple Division.cpp

--- Introductory Problems/Apple Division.cpp
+++ Introductory Problems/Apple Division.cpp
@@ -1,4 +1,33 @@
 #include <bits/stdc++.h>
+using namespace std;
+ 
+// Pattern: we can put a subsequence in one group and the subsequence which is left can be put in another group
+// and for each element we have choices, of putting it into 1st or the 2nd group
+// state definition: minimum difference between two groups with weights grp1Weight and grp2Weight
+// upto the subproblem ith (that is the subproblem prefix upto the ith index)
+long long int minDiffBetwTwoGroupsUptoIth(long long int index, long long int arr[], long long int grp1Weight, long long int grp2Weight){
+    // base case
+    if(index==-1)
+        return llabs(grp1Weight-grp2Weight);
+ 
+    // transition: we have a choice of putting current element in 1st group or 2nd
+    // from both choices, we choose the one which gives the min difference between two groups
+    return min(minDiffBetwTwoGroupsUptoIth(index-1,arr,grp1Weight+arr[index],grp2Weight), minDiffBetwTwoGroupsUptoIth(index-1,arr,grp1Weight,grp2Weight+arr[index]));
+}
+ 
+int main() {
+    long long int i,n;
+    scanf("%lld",&n);
+    long long int arr[n];
+    for(i=0;i<=n-1;i++)
+        scanf("%lld",&arr[i]);
+ 
+    cout<<minDiffBetwTwoGroupsUptoIth(n-1,arr,0,0);
+}
+
+// OLD Solution
+/*
+#include <bits/stdc++.h>
  
 using namespace std;  
 
@@ -24,3 +53,4 @@ int main()
     mndiff(0,a,tsum,sub,diff,n);
     cout<<diff<<endl;
 }
+*/
